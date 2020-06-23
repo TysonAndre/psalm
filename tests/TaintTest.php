5a5f907b35c16e2c70b473338398a2ef2a3f9b12
Brown <github@muglug.com>
2020-06-23 16:53:11 -0400

    Fix #3654 - use correct function id for namespaced functions

--- tests/TaintTest.php
+++ tests/TaintTest.php
@@ -1520,4 +1520,26 @@ class TaintTest extends TestCase
 
         $this->analyzeFile('somefile.php', new Context());
     }
+
+    public function testNamespacedFunction() : void
+    {
+        $this->expectException(\Psalm\Exception\CodeException::class);
+        $this->expectExceptionMessage('TaintedInput');
+
+        $this->project_analyzer->trackTaintedInputs();
+
+        $this->addFile(
+            'somefile.php',
+            '<?php
+                namespace ns;
+
+                function identity(string $s) : string {
+                    return $s;
+                }
+
+                echo identity($_GET[\'userinput\']);'
+        );
+
+        $this->analyzeFile('somefile.php', new Context());
+    }
 }
